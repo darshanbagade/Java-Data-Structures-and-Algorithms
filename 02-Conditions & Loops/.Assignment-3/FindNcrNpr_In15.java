3aab90056b678a9ddb9ab90246b205bb3b49f250
Darshan Bagade <darshanbagade123@gmail.com>
2024-07-02 18:58:56 +0530

    initial commit

---
+++ 02-Conditions & Loops/.Assignment-3/FindNcrNpr_In15.java
@@ -0,0 +1,28 @@
+// 15.Find Ncr and Npr
+import java.util.Scanner;
+public class FindNcrNpr_In15 {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value of n & r (n > r):");  //n > r
+        int n = input.nextInt();
+        int r = input.nextInt();
+        
+        long nCr = (Fact(n)) / (Fact(r)*Fact(n-r));
+        long nPr = (Fact(n) / Fact(n-r));
+
+        System.out.println("nCr = " + nCr);
+        System.out.println("nPr = " + nPr);
+        
+    }
+    static long Fact(int num){
+        long fact=1;
+        if(num==0 || num ==1){        
+            return 1;
+        }
+        for(int i=1 ; i<=num ; i++){
+            fact = fact * i; 
+        }
+        return fact;
+    }
+}
