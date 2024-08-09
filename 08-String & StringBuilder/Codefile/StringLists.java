411ca541971f8dfadcf159f624dddaa417ecba3a
Darshan Bagade <darshanbagade123@gmail.com>
2024-08-09 23:03:47 +0530

    String List

---
+++ 08-String & StringBuilder/Codefile/StringLists.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringLists {
+    public static void main(String[] args) {
+        // create a new ArrayList
+        List<String> myList = new ArrayList<>();
+
+        // add elements to the list using the add() method
+        myList.add("apple");
+        myList.add("banana");
+        myList.add("cherry");
+
+        // print the contents of the list
+        System.out.println(myList);
+        System.out.println(myList.get(0));
+
+
+
+
+        // 2D List-->prefer problem 04 of assignment
+    }
+    
+}
