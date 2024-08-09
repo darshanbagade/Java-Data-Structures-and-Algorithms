4753d6524cf9587f833c189e79647fb507bedb0a
Darshan Bagade <darshanbagade123@gmail.com>
2024-08-09 23:09:23 +0530

    String lists

--- 08-String & StringBuilder/Codefile/StringLists.java
+++ 08-String & StringBuilder/Codefile/StringLists.java
@@ -3,6 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringLists {
+
+    /*
+         List   ---> Lists are used to store an ordered collection of elements that can change the size of a list at runtime. 
+                ---> You can add or remove elements from a list dynamically, which automatically adjusts its size. 
+     */
+
     public static void main(String[] args) {
         // create a new ArrayList
         List<String> myList = new ArrayList<>();
